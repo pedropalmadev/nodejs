d923fc0e26acb603e30a33886d43c4aab942d946
Pedro de Abreu Palma <pedroabreupalma@gmail.com>
2023-07-03 17:39:01 -0300

    seção 5 do curso

---
+++ Seção 5/modules/module-01.js
@@ -0,0 +1,13 @@
+console.log('Executando module-01.js')
+
+const oculta = ()=>{
+    console.log('Executando função oculta')
+}
+
+const executa = ()=>{
+    console.log('Executando a função executa')
+}
+
+welcome = `Bem vindo ao módulo-01.js`
+
+module.exports = {executa, welcome, oculta}
---
+++ Seção 5/modules/module-02.js
@@ -0,0 +1,9 @@
+// const module01 = require("./module-01")
+//module01.executa()
+
+const {executa, oculta} = require("./module-01")
+
+//executando um modulo importado
+executa()
+
+oculta()
